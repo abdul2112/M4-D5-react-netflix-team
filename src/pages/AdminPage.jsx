c114ac17d820cf9cb165818f43a8467d53eef8fa
Sean Knowles <s@seanknowles.dev>
2021-03-26 13:49:08 +0000

    added endpoints to comment service

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -4,6 +4,10 @@ import {
   GET_MOVIES_BY_SEARCH,
   GET_MOVIE_BY_ID
 } from '../services/movies.service'
+import {
+  GET_ALL_COMMENTS,
+  GET_COMMENTS_BY_ELEMENT_ID
+} from '../services/comments.service'
 
 class AdminPage extends Component {
   state = {
@@ -14,8 +18,8 @@ class AdminPage extends Component {
     const movieSearchResult = await GET_MOVIES_BY_SEARCH('pirates') // returns array
     const movie = await GET_MOVIE_BY_ID('tt0325980') // returns single movie
 
-    console.log(movieSearchResult)
-    console.table(movie)
+    const allComments = await GET_ALL_COMMENTS() // returns all comments
+    const commentByElementId = await GET_COMMENTS_BY_ELEMENT_ID('tt0096895') // returns comments associated with an elementId (imdbID)
   }
   render() {
     return (
