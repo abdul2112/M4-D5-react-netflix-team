f47c92b4ba4ba646cf0a71435055847a052d6c1d
baxacsharp <78537820+baxacsharp@users.noreply.github.com>
2021-04-01 22:02:16 +0200

    here is updated ADMIN Page
    age

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -17,184 +17,187 @@ class AdminPage extends React.Component {
     }
   }
 
-}
-submitData = (e) => {
-  e.prevent.default
-  const { name, value } = e.target;
-  let errors = this.state.register;
 
-  switch (name) {
-    case 'name':
-      errors.name =
-        value.length < 3
-          ? 'Name must at least 3  characters long!'
-          : '';
-      break;
+  submitData = (event) => {
+    event.preventDefault()
+    const { name, value } = event.target;
+    let errors = this.state.register;
 
-    case 'Surname':
-      errors.surname =
-        value.length < 2
-          ? 'SurName must at least 2 characters long!'
-          : '';
-      break;
+    switch (name) {
+      case 'name':
+        errors.name =
+          value.length < 3
+            ? 'Name must at least 3  characters long!'
+            : '';
+        break;
+
+      case 'Surname':
+        errors.surname =
+          value.length < 2
+            ? 'SurName must at least 2 characters long!'
+            : '';
+        break;
 
 
-    case 'password':
-      errors.password =
-        value.length < 8
-          ? 'Password must be 8 characters long!'
-          : '';
-      break;
-    case 'YearOfBirth':
-      errors.YearOfBirth =
-        value.length < 1910
-          ? 'Your Birth Year must be Greater than 1910'
-          : '';
-      break;
-    case 'PostalCode':
-      errors.ZipCode =
-        value.length === 5
-          ? 'Zip Code must be 5 characters long!'
-          : '';
-      break;
-    case 'Account':
-      errors.Account =
-        value.length < 16
-          ? 'Account must be 16 characters long!'
-          : '';
-      break;
+      case 'password':
+        errors.password =
+          value.length < 8
+            ? 'Password must be 8 characters long!'
+            : '';
+        break;
+      case 'YearOfBirth':
+        errors.YearOfBirth =
+          value.length < 1910
+            ? 'Your Birth Year must be Greater than 1910'
+            : '';
+        break;
+      case 'PostalCode':
+        errors.ZipCode =
+          value.length === 5
+            ? 'Zip Code must be 5 characters long!'
+            : '';
+        break;
+      case 'Account':
+        errors.Account =
+          value.length < 16
+            ? 'Account must be 16 characters long!'
+            : '';
+        break;
+      default:
+        break;
+
+    }
+  }
+  render() {
+    return (
+      <>
+        <h2 className="text-center">Register to Netflix</h2>
+        <Row className="justify-content-center">
+          <Col xs={6} md={4}>
+            <Form onSubmit={this.submitData} >
+
+              <Form.Group >
+                <Form.Label>Name</Form.Label>
+                <Form.Control onChange={e => this.setState({
+                  register: {
+                    ...this.state.register,
+                    name: e.target.value
+                  }
+                })} value={this.state.register.name} type="text" placeholder="Enter Your Name"
+                  required
+                />
+              </Form.Group>
+
+              <Form.Group >
+                <Form.Label>Surname</Form.Label>
+                <Form.Control onChange={e => this.setState({
+                  register: {
+                    ...this.state.register,
+                    surname: e.target.value
+                  }
+                })} value={this.state.register.surname} type="text" placeholder="Enter Your Surname"
+                  required
+                />
+              </Form.Group>
+              <Form.Group controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control onChange={e => this.setState({
+                  register: {
+                    ...this.state.register,
+                    email: e.target.value
+                  }
+                })} value={this.state.register.email} type="email" placeholder="Enter email"
+                  required
+                />
+                <Form.Text className="text-muted">
+                  We'll never share your email with anyone else.
+              </Form.Text>
+              </Form.Group>
+
+              <Form.Group >
+                <Form.Label>Password</Form.Label>
+                <Form.Control onChange={e => this.setState({
+                  register: {
+                    ...this.state.register,
+                    password: e.target.value
+                  }
+                })} value={this.state.register.password} type="password" placeholder="Password"
+                  required
+                />
+              </Form.Group>
+
+              <Form.Group >
+                <Form.Label>Year Of Birth</Form.Label>
+                <Form.Control onChange={e => this.setState({
+                  register: {
+                    ...this.state.register,
+                    YearOfBirth: e.target.value
+                  }
+                })} value={this.state.register.YearOfBirth} type="number" placeholder="Year of Birth"
+                  required
+                />
+              </Form.Group>
+
+              <Form.Group >
+                <Form.Label>Street Address</Form.Label>
+                <Form.Control onChange={e => this.setState({
+                  register: {
+                    ...this.state.register,
+                    Street: e.target.value
+                  }
+                })} value={this.state.register.Street} type="text" placeholder="Street Address"
+                  required
+                />
+              </Form.Group>
+
+              <Form.Group >
+                <Form.Label>City</Form.Label>
+                <Form.Control onChange={e => this.setState({
+                  register: {
+                    ...this.state.register,
+                    City: e.target.value
+                  }
+                })} value={this.state.register.City} type="text" placeholder="City"
+                  required
+                />
+              </Form.Group>
+
+              <Form.Group >
+                <Form.Label>Zip</Form.Label>
+                <Form.Control onChange={e => this.setState({
+                  register: {
+                    ...this.state.register,
+                    ZipCode: e.target.value
+                  }
+                })} value={this.state.register.ZipCode} type="text" placeholder="Enter your Zip code"
+                  required
+                />
+              </Form.Group>
+
+              <Form.Group >
+                <Form.Label>Enter your Card Number</Form.Label>
+                <Form.Control onChange={e => this.setState({
+                  register: {
+                    ...this.state.register,
+                    Account: e.target.value
+                  }
+                })} value={this.state.register.Account} type="number" placeholder="Credit card account"
+                  required
+                />
+              </Form.Group>
+              <Form.Group controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" label="Check me out" />
+              </Form.Group>
+              <Button variant="primary" type="submit">
+                Submit
+            </Button>
+            </Form >
+          </Col>
+        </Row>
+      </>
+    )
 
   }
-}
-render() {
-  return (
-    <>
-      <h2 className="text-center">Register to Netflix</h2>
-      <Row className="justify-content-center">
-        <Col xs={6} md={4}>
-          <Form onSubmit={this.submitData()} >
-
-            <Form.Group >
-              <Form.Label>Name</Form.Label>
-              <Form.Control onChange={e => this.setState({
-                register: {
-                  ...this.state.register,
-                  name: e.target.value
-                }
-              })} value={this.state.register.name} type="text" placeholder="Enter Your Name"
-                required
-              />
-            </Form.Group>
-
-            <Form.Group >
-              <Form.Label>Surname</Form.Label>
-              <Form.Control onChange={e => this.setState({
-                register: {
-                  ...this.state.register,
-                  surname: e.target.value
-                }
-              })} value={this.state.register.surname} type="text" placeholder="Enter Your Surname"
-                required
-              />
-            </Form.Group>
-            <Form.Group controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control onChange={e => this.setState({
-                register: {
-                  ...this.state.register,
-                  email: e.target.value
-                }
-              })} value={this.state.register.email} type="email" placeholder="Enter email"
-                required
-              />
-              <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-              </Form.Text>
-            </Form.Group>
-
-            <Form.Group >
-              <Form.Label>Password</Form.Label>
-              <Form.Control onChange={e => this.setState({
-                register: {
-                  ...this.state.register,
-                  password: e.target.value
-                }
-              })} value={this.state.register.password} type="password" placeholder="Password"
-                required
-              />
-            </Form.Group>
-
-            <Form.Group >
-              <Form.Label>Year Of Birth</Form.Label>
-              <Form.Control onChange={e => this.setState({
-                register: {
-                  ...this.state.register,
-                  YearOfBirth: e.target.value
-                }
-              })} value={this.state.register.YearOfBirth} type="number" placeholder="Year of Birth"
-                required
-              />
-            </Form.Group>
-
-            <Form.Group >
-              <Form.Label>Street Address</Form.Label>
-              <Form.Control onChange={e => this.setState({
-                register: {
-                  ...this.state.register,
-                  Street: e.target.value
-                }
-              })} value={this.state.register.Street} type="text" placeholder="Street Address"
-                required
-              />
-            </Form.Group>
-
-            <Form.Group >
-              <Form.Label>City</Form.Label>
-              <Form.Control onChange={e => this.setState({
-                register: {
-                  ...this.state.register,
-                  City: e.target.value
-                }
-              })} value={this.state.register.City} type="text" placeholder="City"
-                required
-              />
-            </Form.Group>
-
-            <Form.Group >
-              <Form.Label>Zip</Form.Label>
-              <Form.Control onChange={e => this.setState({
-                register: {
-                  ...this.state.register,
-                  ZipCode: e.target.value
-                }
-              })} value={this.state.register.ZipCode} type="text" placeholder="Enter your Zip code"
-                required
-              />
-            </Form.Group>
-
-            <Form.Group >
-              <Form.Label>Enter your Card Number</Form.Label>
-              <Form.Control onChange={e => this.setState({
-                register: {
-                  ...this.state.register,
-                  Account: e.target.value
-                }
-              })} value={this.state.register.Account} type="number" placeholder="Credit card account"
-                required
-              />
-            </Form.Group>
-            <Form.Group controlId="formBasicCheckbox">
-              <Form.Check type="checkbox" label="Check me out" />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-              Submit
-            </Button>
-          </Form >
-        </Col>
-      </Row>
-    </>
-  )
-
 }
 
 
