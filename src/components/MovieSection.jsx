eba3d8cf0ca4bcef77fe58e5b9cd2bf918bda8bd
“Kravid-Z” <kravidz64@gmail.com>
2021-04-01 02:27:22 +0200

    Spinner in Suggest Content

--- src/components/MovieSection.jsx
+++ src/components/MovieSection.jsx
@@ -1,21 +1,67 @@
 import React from "react";
-import { Row } from "react-bootstrap";
+import { Row, Spinner } from "react-bootstrap";
 import MovieCard from "../components/MovieCard";
 import { GET_MOVIES_BY_SEARCH } from "../services/movies.service";
 
 class MovieSection extends React.Component {
   state = {
     moviesArray: [],
+    isLoading: false,
+    errorAPI: null,
   };
 
   async componentDidMount() {
-    let searchResults = await GET_MOVIES_BY_SEARCH(this.props.suggestContent); // returns array
-    this.setState({ moviesArray: searchResults.Search });
+    try {
+      this.setState({
+        isLoading: true,
+      });
+      let searchResults = await GET_MOVIES_BY_SEARCH(this.props.suggestContent); // returns array
+      if (searchResults.Response === "True") {
+        this.setState({
+          moviesArray: searchResults.Search,
+          isLoading: false,
+        });
+      } else {
+        console.log(searchResults.Error);
+        this.setState({
+          errorAPI: searchResults.Error,
+          isLoading: false,
+        });
+        console.log(this.state.errorAPI);
+      }
+    } catch (error) {
+      console.log(error);
+    }
   }
 
   render() {
     return (
       <>
+        <Row className="justify-content-center align-items-center">
+          {this.state.isLoading && (
+            <>
+              <h2 className="px-4">Loading</h2>
+              <Spinner
+                className="mx-1"
+                animation="grow"
+                variant="light"
+                size="sm"
+              />
+              <Spinner
+                className="mx-1"
+                animation="grow"
+                variant="light"
+                size="sm"
+              />
+              <Spinner
+                className="mx-1"
+                animation="grow"
+                variant="light"
+                size="sm"
+              />
+            </>
+          )}
+        </Row>
         <h2 className="text-white">{this.props.sectionTitle}</h2>
         <Row className="no-gutters row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 mb-4">
           {this.state.moviesArray.slice(0, 4).map((movie) => (
